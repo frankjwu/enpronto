706479ac7bbc2876d15d3662b3991aefc6fe5c12
Frank Wu <fwu2012@gmail.com>
2012-07-26 22:53:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enpronto::Application.config.secret_token = '8106e1e3aa47cf702447a275ac3e93ec76fb949ae6e27c5b5072a45f8c6a333162c6bdee2ae232759c98c423a2203027bf35c3deb5d9aa0d485cd15a88c0950b'
