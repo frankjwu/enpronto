dc9f73f8c0083be617a329ac80dd83c4379dcbc5
Frank Wu <fwu2012@gmail.com>
2012-08-01 21:28:17 -0400

    Revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ Enpronto::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   # Devise configuration
-  config.action_mailer.default_url_options = { :host => 'oliwunpa.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'enpronto.heroku.com' }
 end
